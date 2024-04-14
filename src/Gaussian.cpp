10d0d2cdf02f1faf92cb4a80d988204892d96b9f
mirovix <miroljubmihailovic98@gmail.com>
2024-04-14 11:59:52 +0200

    refactor: semplifaction of methods and removing duplicated code

--- src/Gaussian.cpp
+++ src/Gaussian.cpp
@@ -6,222 +6,182 @@
 namespace rosneuro{
     namespace decoder{
         Gaussian::Gaussian(void) : p_nh_("~"){
-            this->setname("gaussian");
+            this->setName("gaussian");
             this->is_configured_ = false;
         }
 
         Gaussian::~Gaussian(void){}
 
-        std::string Gaussian::path(void){
+        std::string Gaussian::getPath(void){
             this->isSet();
             return this->config_.filename;
         }
 
-        std::vector<int> Gaussian::classes(void){
+        std::vector<int> Gaussian::getClasses(void){
             this->isSet();
-            std::vector<int> classeslbs;
-            for(int i = 0; i < this->config_.classlbs.size(); i++){
-                classeslbs.push_back((int) this->config_.classlbs.at(i));
+            std::vector<int> classes_lbs;
+            for(int i = 0; i < this->config_.class_lbs.size(); i++){
+                classes_lbs.push_back((int) this->config_.class_lbs.at(i));
             }
-            return classeslbs;
+            return classes_lbs;
         }
 
         bool Gaussian::isSet(void){
-            if(this->is_configured_ == false){
-                ROS_ERROR("[%s] Decoder not configured", this->name().c_str());
+            if(!this->is_configured_){
+                ROS_ERROR("[%s] Decoder not configured", this->getName().c_str());
                 return false;
             }
-            
             return this->is_configured_;
         }
 
+        template<typename T>
+        bool Gaussian::getParamAndCheck(const std::string& param_name, T& param_value) {
+            if (!GenericDecoder::getParam(param_name, param_value)) {
+                ROS_ERROR("[%s] Cannot find param '%s'", this->getName().c_str(), param_name.c_str());
+                return false;
+            }
+            return true;
+        }
+
         bool Gaussian::configure(void){
-            // get the parameters
-            if(!GenericDecoder::getParam(std::string("filename"), this->config_.filename)){
-                ROS_ERROR("[%s] Cannot find param 'filename'", this->name().c_str());
-                return false;
-            }
-            if(!GenericDecoder::getParam(std::string("subject"), this->config_.subject)){
-                ROS_ERROR("[%s] Cannot find param 'subject'", this->name().c_str());
-                return false;
-            }
-            if(!GenericDecoder::getParam(std::string("nclasses"), this->config_.nclasses)){
-                ROS_ERROR("[%s] Cannot find param 'nclasses'", this->name().c_str());
-                return false;
-            }
-            if(!GenericDecoder::getParam(std::string("classlbs"), this->config_.classlbs)){
-                ROS_ERROR("[%s] Cannot find param 'classlbs'", this->name().c_str());
-                return false;
-            }
-            if(!GenericDecoder::getParam(std::string("nprototypes"), this->config_.nprototypes)){
-                ROS_ERROR("[%s] Cannot find param 'nprototypes'", this->name().c_str());
-                return false;
-            }
-            if(!GenericDecoder::getParam(std::string("nfeatures"), this->config_.nfeatures)){
-                ROS_ERROR("[%s] Cannot find param 'nfeatures'", this->name().c_str());
-                return false;
-            }
-            if(!GenericDecoder::getParam(std::string("idchans"), this->config_.idchans)){
-                ROS_ERROR("[%s] Cannot find param 'idchans'", this->name().c_str());
-                return false;
-            }
-            std::string freqs_str;
-            if(!GenericDecoder::getParam(std::string("freqs"), freqs_str)){
-                ROS_ERROR("[%s] Cannot find param 'freqs'", this->name().c_str());
-                return false;
-            }
-            if(!this->load_vectorOfVector(freqs_str, this->config_.freqs)){
-                ROS_ERROR("[%s] Cannot convert param 'freqs' to vctor of vector", this->name().c_str());
-                return false;
-            }
-            std::string centers_str, covs_str;
-            if(!GenericDecoder::getParam(std::string("centers"), centers_str)){
-                ROS_ERROR("[%s] Cannot find param 'centers'", this->name().c_str());
-                return false;
-            }
-            this->centers_ = Eigen::MatrixXf::Zero(this->config_.nfeatures*this->config_.nclasses, this->config_.nprototypes);
-            if(!this->load_eigen(centers_str, this->centers_)){
-                ROS_ERROR("[%s] Failed to load eigen matrix for centers", this->name().c_str());
-                return false;
-            }
-            if(!GenericDecoder::getParam(std::string("covs"), covs_str)){
-                ROS_ERROR("[%s] Cannot find param 'covs'", this->name().c_str());
-                return false;
-            }
-            this->covs_ = Eigen::MatrixXf::Zero(this->config_.nfeatures*this->config_.nclasses, this->config_.nprototypes);
-            if(!this->load_eigen(covs_str, this->covs_)){
-                ROS_ERROR("[%s] Failed to load eigen matrix for covs", this->name().c_str());
+            if (!getParamAndCheck("filename", this->config_.filename)) return false;
+            if (!getParamAndCheck("subject", this->config_.subject)) return false;
+            if (!getParamAndCheck("n_classes", this->config_.n_classes)) return false;
+            if (!getParamAndCheck("class_lbs", this->config_.class_lbs)) return false;
+            if (!getParamAndCheck("n_prototypes", this->config_.n_prototypes)) return false;
+            if (!getParamAndCheck("n_features", this->config_.n_features)) return false;
+            if (!getParamAndCheck("idchans", this->config_.idchans)) return false;
+
+            std::string freqs_str, centers_str, covs_str;
+            if (!getParamAndCheck("freqs", freqs_str)) return false;
+            if(!this->loadVectorOfVector(freqs_str, this->config_.freqs)){
+                ROS_ERROR("[%s] Cannot convert param 'freqs' to vector of vector", this->getName().c_str());
                 return false;
             }
 
-            // fast check for the correct dimension for the features
-            if(!this->check_dimension()){
-                ROS_ERROR("[%s] Error in the dimension", this->name().c_str());
+            if (!getParamAndCheck("centers", centers_str)) return false;
+            this->centers_ = Eigen::MatrixXf::Zero(this->config_.n_features*this->config_.n_classes, this->config_.n_prototypes);
+            if(!this->loadEigen(centers_str, this->centers_)){
+                ROS_ERROR("[%s] Failed to load eigen matrix for centers", this->getName().c_str());
+                return false;
+            }
+
+            if (!getParamAndCheck("covs", covs_str)) return false;
+            this->covs_ = Eigen::MatrixXf::Zero(this->config_.n_features*this->config_.n_classes, this->config_.n_prototypes);
+            if(!this->loadEigen(covs_str, this->covs_)){
+                ROS_ERROR("[%s] Failed to load eigen matrix for covs", this->getName().c_str());
+                return false;
+            }
+
+            if(!this->checkDimension()){
+                ROS_ERROR("[%s] Error in the dimension", this->getName().c_str());
                 return false;
             }
 
             this->is_configured_ = true;
-
             return this->is_configured_;
         }
 
-        Eigen::VectorXf Gaussian::apply(const Eigen::VectorXf& in){
-
-            // check if set and correct dimension input
-            Eigen::VectorXf out(this->config_.nclasses);
-            this->isSet();
-            if(in.size() != this->config_.nfeatures){
-                ROS_ERROR("[%s] input incorrect dimension", this->name().c_str());
+        Eigen::VectorXf Gaussian::apply(const Eigen::VectorXf& input) {
+            Eigen::VectorXf output(this->config_.n_classes);
+            if (input.size() != this->config_.n_features) {
+                ROS_ERROR("[%s] input incorrect dimension", getName().c_str());
+                return output;
             }
 
-            // create variables
-            unsigned int modelRows  = this->config_.nclasses;
-            unsigned int modelCols  = this->config_.nprototypes;
-            unsigned int modelFeats = in.size();
+            unsigned int modelRows = this->config_.n_classes;
+            unsigned int modelCols = this->config_.n_prototypes;
+            unsigned int modelFeats = input.size();
 
-            Eigen::MatrixXf subModelM = Eigen::MatrixXf::Zero(modelFeats, modelCols);
-            Eigen::MatrixXf subModelC = Eigen::MatrixXf::Zero(modelFeats, modelCols);
+            Eigen::MatrixXf activities = computeActivities(input, modelRows, modelCols, modelFeats);
+            const Eigen::VectorXf rawProbs = computeRawProbabilities(activities);
+            updateOutput(output, rawProbs, rawProbs.sum(), modelRows);
 
-            Eigen::VectorXf M        = Eigen::VectorXf::Zero(modelFeats);
-            Eigen::VectorXf C        = Eigen::VectorXf::Zero(modelFeats);
-            Eigen::VectorXf distance = Eigen::VectorXf::Zero(modelFeats);
-            Eigen::VectorXf tempDet  = Eigen::VectorXf::Zero(modelFeats);
+            return output;
+        }
 
-            double determinant = 0.0;
-            double distanceSum = 0.0;
-            double sumProbs    = 0.0;
-
+        Eigen::MatrixXf Gaussian::computeActivities(const Eigen::VectorXf& input, unsigned int modelRows, unsigned int modelCols, unsigned int modelFeats) {
             Eigen::MatrixXf activities = Eigen::MatrixXf::Zero(modelRows, modelCols);
-            Eigen::VectorXf rawProbs = Eigen::VectorXf::Zero(modelRows);
 
-            // iterate over classes
-            for(unsigned int i = 0; i < modelRows; i++){
-                subModelM = this->centers_.block(i*modelFeats, 0, modelFeats, modelCols);
-                subModelC = this->covs_.block(i*modelFeats, 0, modelFeats, modelCols);
+            for (unsigned int i = 0; i < modelRows; ++i) {
+                const Eigen::MatrixXf subModelM = this->centers_.block(i * modelFeats, 0, modelFeats, modelCols);
+                const Eigen::MatrixXf subModelC = this->covs_.block(i * modelFeats, 0, modelFeats, modelCols);
 
-                // iterate over prototypes
-                for(unsigned int j = 0; j < modelCols; j++){
-                    M = subModelM.col(j);
-                    C = subModelC.col(j);
+                for (unsigned int j = 0; j < modelCols; ++j) {
+                    const Eigen::VectorXf M = subModelM.col(j);
+                    const Eigen::VectorXf C = subModelC.col(j);
 
-                    distance = (in - M).array().pow(2) / C.array();
-                    tempDet  = C.array().sqrt();
+                    const Eigen::VectorXf distance = computeDistance(input, M, C);
+                    const Eigen::VectorXf tempDet = computeTempDet(C);
+                    const double determinant = tempDet.prod();
+                    const double distanceSum = distance.sum();
 
-                    for(unsigned int u = 0; u < tempDet.size(); u++){
-                        if(tempDet(u) == 0){
-                            tempDet(u) = 1;
-                        }
-                    }
-
-                    determinant = tempDet.array().prod();
-
-                    distanceSum = distance.array().sum();
-
-                    activities(i,j) = exp(-(distanceSum/2.0)) / determinant;
-                
+                    activities(i, j) = exp(-(distanceSum / 2.0)) / determinant;
                 }
             }
 
-            rawProbs = activities.rowwise().sum();
-            
-            sumProbs = rawProbs.array().sum();
-
-            if(sumProbs == 0){
-                out = rawProbs.array().setOnes() / modelRows;
-            }else{
-                out = rawProbs.array() / sumProbs;
-            }
-
-
-            return out;
+            return activities;
         }
-        
+
+        Eigen::VectorXf Gaussian::computeDistance(const Eigen::VectorXf& input, const Eigen::VectorXf& M, const Eigen::VectorXf& C) {
+            return (input - M).array().pow(2) / C.array();
+        }
+
+        Eigen::VectorXf Gaussian::computeTempDet(const Eigen::VectorXf& C) {
+            return C.array().sqrt().unaryExpr([](float val) { return val == 0 ? 1 : val; });
+        }
+
+        Eigen::VectorXf Gaussian::computeRawProbabilities(const Eigen::MatrixXf& activities) {
+            return activities.rowwise().sum();
+        }
+
+        void Gaussian::updateOutput(Eigen::VectorXf& output, const Eigen::VectorXf& rawProbs, double sumProbs, unsigned int modelRows) {
+            if (sumProbs == 0) {
+                output.setConstant(1.0 / modelRows);
+            } else {
+                output = rawProbs / sumProbs;
+            }
+        }
+
 		Eigen::VectorXf Gaussian::getFeatures(const Eigen::MatrixXf& in){
-            // check if set and prepare for the correct dimension
-            Eigen::VectorXf out(this->config_.nfeatures);
+            Eigen::VectorXf out(this->config_.n_features);
             this->isSet();
 
-            // iterate over channels
             int c_feature = 0;
             for(int it_chan = 0; it_chan < this->config_.idchans.size(); it_chan++){
-                int idchan = this->config_.idchans.at(it_chan) - 1; // -1 bc: channels starts from 1 and not 0
-                // iterate over freqs for that channel
+                int id_chan = this->config_.idchans.at(it_chan) - 1;
                 for(const auto& freq : this->config_.freqs.at(it_chan)){
-                    // we have the freq value and not the id of that freq
-                    int idfreq = (int) freq/2.0;
-                    out(c_feature) = in(idchan, idfreq);
+                    out(c_feature) = in(id_chan, (int) freq/2.0);
                     c_feature ++;
                 }
             }
-
             return out.transpose();
         }
 
+        bool Gaussian::checkDimension() {
+            const auto& idchans = this->config_.idchans;
+            const auto& freqs = this->config_.freqs;
+            const auto& n_features = this->config_.n_features;
 
-        
-
-        // fast check for the features used by the decoder
-        bool Gaussian::check_dimension(void){
-            if(this->config_.idchans.size() != this->config_.freqs.size()){
-                ROS_ERROR("[%s] Incorrect dimension for 'freqs' and 'idchans'", this->name().c_str());
+            if (idchans.size() != freqs.size()) {
+                ROS_ERROR("[%s] Incorrect dimension for 'freqs' and 'idchans'", this->getName().c_str());
                 return false;
             }
 
-            int sum = 0;
-            for(int i = 0; i < this->config_.freqs.size(); i++){
-                std::vector<uint32_t> temp = this->config_.freqs.at(i);
-                sum += temp.size();
+            int totalFreqs = 0;
+            for (const auto& freq : freqs) {
+                totalFreqs += freq.size();
             }
-            if(sum != this->config_.nfeatures){
-                ROS_ERROR("[%s] Incorrect dimension for 'freqs' different from 'nfeatures'", this->name().c_str());
+
+            if (totalFreqs != n_features) {
+                ROS_ERROR("[%s] Incorrect dimension for 'freqs' different from 'n_features'", this->getName().c_str());
                 return false;
             }
 
             return true;
         }
 
-        
-PLUGINLIB_EXPORT_CLASS(rosneuro::decoder::Gaussian, rosneuro::decoder::GenericDecoder);
+        PLUGINLIB_EXPORT_CLASS(rosneuro::decoder::Gaussian, rosneuro::decoder::GenericDecoder);
     }
 }
 #endif
