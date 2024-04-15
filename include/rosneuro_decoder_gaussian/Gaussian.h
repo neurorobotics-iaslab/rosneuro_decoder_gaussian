fb20b81c0258aab36f7939e91acfbd550beb8853
mirovix <miroljubmihailovic98@gmail.com>
2024-04-15 22:27:27 +0200

    feat: refactor header

--- include/rosneuro_decoder_gaussian/Gaussian.h
+++ include/rosneuro_decoder_gaussian/Gaussian.h
@@ -5,47 +5,54 @@
 #include <dynamic_reconfigure/server.h>
 #include <ros/ros.h>
 #include <gtest/gtest_prod.h>
+#include <unordered_map>
 #include "rosneuro_decoder/GenericDecoder.h"
 
 namespace rosneuro{
     namespace decoder{
 
         typedef struct {
-
         	std::string 		   filename;
-
         	std::string		       subject;
-        	std::uint32_t		   nclasses;
-        	std::vector<uint32_t>  classlbs;
-        	std::uint32_t		   nprototypes;
-        	std::uint32_t      	   nfeatures;
+        	std::uint32_t		   n_classes;
+        	std::vector<uint32_t>  class_lbs;
+        	std::uint32_t		   n_prototypes;
+        	std::uint32_t      	   n_features;
 
-            // for features extraction
         	std::vector<uint32_t>               idchans;
         	std::vector<std::vector<uint32_t>>  freqs;
 
-        } gauconfig_t;
+        } gaussian_config;
         
 
         class Gaussian : public GenericDecoder{
             public:
                 Gaussian(void);
                 ~Gaussian(void);
+
                 bool configure(void);
                 bool isSet(void);
                 Eigen::VectorXf apply(const Eigen::VectorXf& in);
 		        Eigen::VectorXf getFeatures(const Eigen::MatrixXf& in);
-                std::string path(void);
-		        std::vector<int> classes(void);
+                std::string getPath(void);
+		        std::vector<int> getClasses(void);
 
             private:
-                bool check_dimension(void);
+                std::tuple<unsigned int, unsigned int, unsigned int> getModelParameters();
+                Eigen::MatrixXf computeActivities(const Eigen::VectorXf& input, unsigned int modelRows, unsigned int modelCols, unsigned int modelFeats);
+                Eigen::VectorXf computeDistance(const Eigen::VectorXf& input, const Eigen::VectorXf& M, const Eigen::VectorXf& C);
+                Eigen::VectorXf computeTempDet(const Eigen::VectorXf& C);
+                Eigen::VectorXf computeRawProbabilities(const Eigen::MatrixXf& activities);
+                void updateOutput(Eigen::VectorXf& output, const Eigen::VectorXf& rawProbs, double sumProbs, unsigned int modelRows);
+                bool checkDimension(void);
+                template<typename T>
+                bool getParamAndCheck(const std::string& param_name, T& param_value);
 
-            private:
-		        gauconfig_t config_;
+                gaussian_config config_;
                 ros::NodeHandle p_nh_;
-                Eigen::MatrixXf centers_; // [(features*nclasses) x nprototypes]
-                Eigen::MatrixXf covs_; // [(features*nclasses) x nprototypes]
+                Eigen::MatrixXf centers_;
+                Eigen::MatrixXf covs_;
+
                 FRIEND_TEST(GaussianTestSuite, Constructor);
                 FRIEND_TEST(GaussianTestSuite, Path);
                 FRIEND_TEST(GaussianTestSuite, Classes);
@@ -54,6 +61,7 @@ namespace rosneuro{
                 FRIEND_TEST(GaussianTestSuite, CheckDimensionFreq);
                 FRIEND_TEST(GaussianTestSuite, GetFeatures);
                 FRIEND_TEST(GaussianTestSuite, Apply);
+                FRIEND_TEST(GaussianTestSuite, Integration);
         };
     }
 }
